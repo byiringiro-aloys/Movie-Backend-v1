1a3102f9f4d10acf16254ed4b074df760199f20b
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-08-06 10:00:32 +0200

    Added configurations for cors.

---
+++ src/main/java/dev/aloys/Movies/config/ConfigurationFile.java
@@ -0,0 +1,25 @@
+package dev.aloys.Movies.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigurationFile {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all routes
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}

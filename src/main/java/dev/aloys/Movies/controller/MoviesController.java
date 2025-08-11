4e7f1cd5a5118f09c6bd69de0b569a017d084f43
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-08-11 20:22:45 +0200

    Removed unnecessary imports

--- src/main/java/dev/aloys/Movies/controller/MoviesController.java
+++ src/main/java/dev/aloys/Movies/controller/MoviesController.java
@@ -1,9 +1,7 @@
 package dev.aloys.Movies.controller;
 
 import dev.aloys.Movies.model.Movies;
-import dev.aloys.Movies.repository.MoviesRepository;
 import dev.aloys.Movies.service.MovieService;
-import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +11,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MoviesController {
+
     private MovieService movieService;
+
     public MoviesController(MovieService movieService) {
         this.movieService = movieService;
     }
+
     @GetMapping
     public ResponseEntity<List<Movies>> getAllMovies(){
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
